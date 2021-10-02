cc972147885c822651baf5c92c3a4342c5a133c4
evilya2505 <like17seventeen@yandex.ru>
2021-10-02 23:03:09 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -40,10 +40,9 @@ app.use('/', (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+  res.header('Access-Control-Allow-Origin', origin);
+
 
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
